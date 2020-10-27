7373dbbeeadb3158b4484d021f730dc9ed73e6a1
Ryan Williamson <w.rjwilliamson@gmail.com>
2020-10-27 15:14:57 +0000

    Finished treeNodeCount and treeDepth.

--- FT.hs
+++ FT.hs
@@ -3,13 +3,16 @@ module FT where
 data FunnyTree a = FunnyNode a [FunnyTree a] deriving (Show,Eq)
 
 treeNodeCount :: FunnyTree a -> Int
-treeNodeCount _ = 0
+treeNodeCount (FunnyNode _ []) = 1
+treeNodeCount (FunnyNode _ subTree) = 1 + sum (map treeNodeCount subTree)
 
 treeDepth :: FunnyTree a -> Int
-treeDepth _ = 0
+treeDepth (FunnyNode _ []) = 1
+treeDepth (FunnyNode _ subTree) = 1 + maximum (map treeDepth subTree)
 
 treeMap :: (a->b) -> FunnyTree a -> FunnyTree b
 treeMap f (FunnyNode x _) = FunnyNode (f x) []
 
+-- Depth first fold
 treeFold :: (a->b->b) -> b -> FunnyTree a -> b
-treeFold f z ft = z
+treeFold fun acc tree = acc
