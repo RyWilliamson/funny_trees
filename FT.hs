c22aa5ab0ea039bac32669fb15604fcb6d83b967
Ryan Williamson <w.rjwilliamson@gmail.com>
2020-10-27 16:48:49 +0000

    Added map and tree code.

--- FT.hs
+++ FT.hs
@@ -11,8 +11,13 @@ treeDepth (FunnyNode _ []) = 1
 treeDepth (FunnyNode _ subTree) = 1 + maximum (map treeDepth subTree)
 
 treeMap :: (a->b) -> FunnyTree a -> FunnyTree b
-treeMap f (FunnyNode x _) = FunnyNode (f x) []
+treeMap f (FunnyNode x []) = FunnyNode (f x) []
+treeMap f (FunnyNode x subTree) = FunnyNode (f x) (map (treeMap f) subTree)
 
 -- Depth first fold
 treeFold :: (a->b->b) -> b -> FunnyTree a -> b
-treeFold fun acc tree = acc
+treeFold f acc tree = foldr f acc (treeFlatten tree)
+
+treeFlatten :: FunnyTree a -> [a]
+treeFlatten (FunnyNode a []) = [a]
+treeFlatten (FunnyNode a subTree) = a: concat (map treeFlatten subTree)
